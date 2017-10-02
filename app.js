61027eca9aae7dd4a95cf67c926773e83af705c5
Leonardo Victor <leonardovff@gmail.com>
2017-10-02 09:55:16 -0300

    Add the js to like

---
+++ app.js
@@ -0,0 +1,17 @@
+var interval = null;
+function run(time){
+	var qtd = 0, 
+	btn = document.querySelector('button[aria-label="Curti"]');
+	if(typeof interval != null)
+		clearInterval(interval);
+	interval = setInterval(() => {
+        qtd++;
+        console.log(qtd);	
+        btn.click()
+    }, time);
+}
+function stop(){
+	clearInterval(interval);
+}
+//time in milliseconds
+//run(200)
