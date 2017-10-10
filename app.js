0387810f356a91e7e26356a55e5dc9221bb0f000
André Van Dal <derevandal@users.noreply.github.com>
2017-10-10 18:55:50 -0300

    change if statement to try...catch and fix card's name on log

--- app.js
+++ app.js
@@ -1,70 +1,71 @@
-var interval = null;
+var interval = null,
+	qtd = 0;
 
 function run(time = 300, KEYWORDS = false){
-	let qtd = 0,
-	isDislike = false,
-	selectorBtn,
-	btn;
-
 	if(typeof interval !== null){
 		clearInterval(interval);
 	}
+	interval = setInterval(action(KEYWORDS), time);
+}
 
-	interval = setInterval(() => {
-		let filtered = false;
-		if(Array.isArray(KEYWORDS)){
-			filtered = filter(KEYWORDS);
-			isDislike = (filtered.qtd > 0)
-		}
+function action(KEYWORDS = false){
+	qtd += 1;
+
+	let isDislike = false,
+		selectorBtn,
+		btn,
+		cardActived,
+		filtered = false,
+		msg = "["+ qtd + "] ";
+
+	try {
+		cardActived = document.querySelector("div.recCard.needsclick.active");
+
+		filtered = filter(cardActived, KEYWORDS);
+		isDislike = (filtered.qtd > 0);
 		selectorBtn = isDislike ? 'button.recsGamepad__button--dislike' : 'button.recsGamepad__button--like';
 		btn = document.querySelector(selectorBtn);
-		if(typeof btn !== null) {
-			qtd++;
-			let cardNameAge = document.querySelector("div.recCard__nameAge")
-			let msg = "["+ qtd + "] ";
-			if(cardNameAge){
-				cardNameAge = cardNameAge.textContent.split(', ');
-				let cardName = cardNameAge[0];
-				let cardAge = cardNameAge[1];
-				msg += (isDislike? 'Dislike' : 'Like') + ' at ' + cardName + " ("+ cardAge +")."
-				if(filtered && filtered.qtd > 0 && filtered.words){
-					console.log(filtered)
-					msg += "\n\t- "+filtered.qtd + " Filtered words: " + filtered.words.join(', ');
-				}
-				btn.click()
-			} else {
-				msg = 'No cards found.'
-			}
-			console.log(msg)
+
+		cardNameAge = cardActived.querySelector("div.recCard__nameAge").textContent.split(', ');
+		cardNameAge = {
+			"name": cardNameAge[0],
+			"age": cardNameAge[1]
 		}
+		msg += (isDislike? 'Dislike' : 'Like') + ' at ' + cardNameAge.name + " ("+ cardNameAge.age +")."
+		if(filtered){
+			msg += "\n\t- "+filtered.qtd + " Filtered words: " + filtered.words.join(', ');
+		}
+		btn.click()
+	} catch (e) {
+		msg = e
+	} finally {
+		console.log(msg)
+	}
 
-	}, time);
 }
 
-function stop(){
-	console.log('stopped!')
-	clearInterval(interval);
-}
-
-function filter(KEYWORDS){
-	const openProfile = document.querySelector('div.recCard__openProfile');
-	let filtered = false
-	if(openProfile)  {
-		filtered = {}
-		openProfile.click();
+function filter(cardActived, KEYWORDS){
+	try {
+		const click = cardActived.querySelector('div.recCard__openProfile').click();
 		const profileDescription = document.querySelector("div.profileCard__textContent");
 		const words = KEYWORDS.filter(word => {
 			return profileDescription.textContent.toUpperCase().includes(word.toUpperCase());
 		})
-		const qtd = words.length;
-		filtered.words = words;
-		filtered.qtd = qtd;
-		document.querySelector('a.profileCard__backArrow').click()
-
-	} else {
-		stop();
+		const close = document.querySelector('a.profileCard__backArrow').click()
+		return {
+			"qtd": words.length,
+			"words": words
+		}
+	} catch (e) {
+		console.log(e)
+		return false;
 	}
-	return filtered;
+}
+
+
+function stop(){
+	console.log('stopped!')
+	clearInterval(interval);
 }
 
 //time in milliseconds
